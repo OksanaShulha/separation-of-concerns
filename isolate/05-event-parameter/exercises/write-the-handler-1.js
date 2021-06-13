a26dffd7da22f7c2c3433fc602f1b1b626e2d52e
Oksana Shulha <shushullja@gmail.com>
2021-06-13 16:28:19 +0200

    Add faq

--- isolate/05-event-parameter/exercises/write-the-handler-1.js
+++ isolate/05-event-parameter/exercises/write-the-handler-1.js
@@ -13,11 +13,8 @@ const target = event.target;
 const innerText = event.target.innerHTML;
 let reverseText = '';
 console.log(innerText);
-<<<<<<< HEAD
-for (let i = innerText.length - 1; i >= 0; i--) {
-=======
+
 for ( let i = innerText.length - 1; i >= 0; i--) {
->>>>>>> 40279ae6f78ae11580c9fc45bc23e255025a161e
 reverseText += innerText[i];
 }
 console.log(reverseText);
